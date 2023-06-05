1c278d00d0cfa31d0376c85c31a572fc63d4d09a
afrin-titly <titlee42@gmail.com>
2023-06-05 22:12:31 +0900

    add csv download option

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import sys
+import csv
+import os
 
 start_time = sys.argv[1]
 end_time = sys.argv[2]
@@ -6,3 +8,19 @@ end_time = sys.argv[2]
 print("Hello World!!")
 print("Start time: " + start_time)
 print("End time: " + end_time)
+
+path = "csv"
+isExist = os.path.exists("csv")
+if not isExist:
+   # Create a new directory because it does not exist
+   os.makedirs(path)
+   print("The new directory is created!")
+
+with open('./csv/profiles.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    field = ["name", "age", "country"]
+
+    writer.writerow(field)
+    writer.writerow(["Oladele Damilola", "40", "Nigeria"])
+    writer.writerow(["Alina Hricko", "23", "Ukraine"])
+    writer.writerow(["Isabel Walter", "50", "United Kingdom"])
